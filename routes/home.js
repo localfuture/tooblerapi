83092085d3c3b0e56eb659dab7a81ab913fdb83b
root <root@LAPTOP-QLDHK0FC.localdomain>
2021-04-09 18:29:18 +0530

    pages complete

---
+++ routes/home.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home');
+});
+
+router.get('/settings', function(req, res, next) {
+  res.render('settings');
+});
+
+module.exports = router;
